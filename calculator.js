ce9475201344fe679d3b2ce55a5b9529b78ea1ee
porkowski <borkowskipatryk10@gmail.com>
2023-06-03 20:28:23 -0400

    Calculator works properly and displays results.

--- calculator.js
+++ calculator.js
@@ -1,8 +1,13 @@
 //Functions for add, subtract, multiply and divide.
 
 
-function operate(num1,operator,num2) {
-    //take string such as '4*5' and split into three variables
+function operate(str) {
+    let parameters = str.split(' ');
+    console.log(parameters);
+    let num1 = parseInt(parameters[0]);
+    let operator = parameters[1];
+    let num2 = parseInt(parameters[2]);
+   
 
     switch(operator) {
     case '+':
@@ -20,8 +25,24 @@ function operate(num1,operator,num2) {
 }
 }
 
+//grab display to DOM
 
-let buttons = document.querySelectorAll('.btn')
-buttons.addEventListener("mousedown", function (e) {
-    console.log("hello")
+let display = document.querySelector('span');
+display.textContent = '';
+
+
+
+let buttons = document.querySelectorAll('.btn');
+buttons.forEach((button)=> {
+button.addEventListener("mousedown",function() {
+
+
+if (button.textContent == '=') {
+    display.textContent = operate(display.textContent);
+} else if (button.textContent == 'CLEAR') {
+    display.textContent = '';
+} else {
+    display.textContent += button.textContent;
+}
 })
+});
